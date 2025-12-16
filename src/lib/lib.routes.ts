95866aba8c99a9ad26d8cefe639ea95a261ab24e
taurus366 <taurus.ali47@gmail.com>
2025-12-16 11:09:00 +0200

    chore: update readme and tsconfig

---
+++ src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { XlUtil } from './xl-util/xl-util';
+
+export const xlUtilRoutes: Route[] = [{ path: '', component: XlUtil }];
---
+++ src/lib/xl-util/xl-util.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'lib-xl-util',
+    imports: [],
+    templateUrl: './xl-util.html',
+    styleUrl: './xl-util.css'
+})
+export class XlUtil {}
