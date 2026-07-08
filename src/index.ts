80085e7aff2f9dcab602c3d93eccefa642ba23e7
developer <taurus.ali47@gmail.com>
2026-07-08 15:55:12 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/crud/BaseListCrud';
 export * from './lib/crud/BaseDetailCrud';
 export * from './lib/menu/menu-store';
 export * from './lib/menu/route-store';
+export * from './lib/menu/menu-badge';
 
 export * from './lib/auth_guard_bridge/auth_guard';
 export * from './lib/layout_bridge/topbar';
